b3bf123cd5ee9c1bf1d76434a25024ade4b307e8
Paulojosep <paulo.jmodesto@htmail.com>
2021-05-10 23:13:23 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dscaralog/DscaralogApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscaralog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscaralogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscaralogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dscaralog/DscaralogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscaralog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscaralogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
