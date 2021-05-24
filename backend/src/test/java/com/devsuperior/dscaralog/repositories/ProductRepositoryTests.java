835f9c8eab76029c7c222d171714429c9dc4ee92
Paulojosep <paulo.jmodesto@htmail.com>
2021-05-24 09:58:08 -0300

    Primeiro teste de repository

---
+++ backend/src/test/java/com/devsuperior/dscaralog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,29 @@
+package com.devsuperior.dscaralog.repositories;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.devsuperior.dscaralog.entities.Product;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	@Test
+	public void deleteShouldDeletObjectWhenIdExists() {
+		
+		Long exintingId = 1L;
+		
+		repository.deleteById(exintingId);
+		
+		Optional<Product> result = repository.findById(exintingId);
+		Assertions.assertFalse(result.isPresent());
+	}
+	
+}
