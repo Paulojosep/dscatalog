3f75f97c01f638c2212c719a2933cd28f2b4fc73
Paulojosep <paulo.jmodesto@htmail.com>
2021-06-02 11:21:18 -0300

    Basic bean validation

--- backend/src/main/java/com/devsuperior/dscaralog/dto/UserDTO.java
+++ backend/src/main/java/com/devsuperior/dscaralog/dto/UserDTO.java
@@ -4,14 +4,23 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 import com.devsuperior.dscaralog.entities.User;
 
 public class UserDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
+	
+	@NotBlank(message = "Campo obrigatorio")
 	private String firstName;
+	
+	@NotBlank(message = "Campo obrigatorio")
 	private String lastName;
+	
+	@Email(message = "Favor entrar um email válido")
 	private String email;
 	
 	Set<RoleDTO> roles = new HashSet<>();
