632330f77f3c822ac8da12669a6281bd277196ca
Paulojosep <paulo.jmodesto@htmail.com>
2021-06-02 12:39:02 -0300

    Custom ConstraintValidator

--- backend/src/main/java/com/devsuperior/dscaralog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/devsuperior/dscaralog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.devsuperior.dscaralog.dto;
 
+import com.devsuperior.dscaralog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO{
 	private static final long serialVersionUID = 1L;
 	
