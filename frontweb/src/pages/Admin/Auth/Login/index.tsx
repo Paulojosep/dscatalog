c10caa23283304ac40490ac0f53c310906585c61
Paulojosep <paulo.jmodesto@htmail.com>
2021-07-27 18:28:21 -0300

    Redirecting to intended route after login

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -1,11 +1,7 @@
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import ButtonIcon from '../../../../components/ButtonIcon';
 import { useForm } from 'react-hook-form';
-import {
-  getTokenData,
-  requestBackendLogin,
-  saveAuthData,
-} from '../../../../util/request';
+import { getTokenData, requestBackendLogin, saveAuthData } from '../../../../util/request';
 import { useContext, useState } from 'react';
 import { AuthContext } from '../../../../AuthContext';
 import './styles.css';
@@ -15,7 +11,16 @@ type FormData = {
   password: string;
 };
 
+type LocationState = {
+  from: string;
+}
+
 const Login = () => {
+
+  const location = useLocation<LocationState>();
+
+  const { from } = location.state || { from: { pathname: '/admin' } }
+
   const { setAuthContextData } = useContext(AuthContext);
 
   const [hasError, setHasError] = useState(false);
@@ -37,7 +42,7 @@ const Login = () => {
           authenticated: true,
           tokenData: getTokenData(),
         });
-        history.push('/admin');
+        history.replace(from);
       })
       .catch((error) => {
         setHasError(true);
