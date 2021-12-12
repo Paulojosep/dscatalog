fe39677e01ec8173eed7f039dc71e608df135433
paulojosep <paulo.jmodesto@hotmail.com>
2021-12-12 10:08:07 -0300

    Catalog, waitFor, async-await

---
+++ frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -0,0 +1,19 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import Catalog from '../index';
+import history from '../../../util/history';
+
+test('shoul render Catalog with given products', async () => {
+
+  render(
+    <Router history={history}>
+        <Catalog />
+    </Router>
+  );
+
+  expect(screen.getByText('Catálogo de produtos')).toBeInTheDocument();
+  
+  await waitFor(() => {
+      expect(screen.getByText("Smart TV")).toBeInTheDocument();
+  });
+});
